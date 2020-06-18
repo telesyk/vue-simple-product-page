c988aa3d63ae926b6aa9a89fb7152ac6b450f229
telesyk <oles.tkachuk@yola.com>
2020-06-18 23:49:21 +0300

    Add computed methods image, sizes, inventory, stockLabelClass.
    Refactored methods.

--- index.js
+++ index.js
@@ -3,78 +3,83 @@ new Vue({
 
   data: {
     cart: 0,
-    product: {
-      title: 'Socks',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.',
-      assets: 'https://images.vans.com/is/image/Vans/XRZHTG-HERO?$583x583$',
-      inventory: 10,
-      onSale: true,
-      label: '',
-      details: [
-        '80 cotton', '20% polyester', 'Gender-neutral'
-      ],
-      tabs: [
-        {
-          id: 112,
-          title: 'Details',
-          active: true,
-        }, {
-          id: 113,
-          title: 'Color & Size',
-          active: false,
-        }
-      ],
-      variants: [
-        {
-          id: 2234,
-          color: 'grey',
-          image: 'https://images.vans.com/is/image/Vans/XRZHTG-HERO?$583x583$',
-        },
-        {
-          id: 2235,
-          color: 'blue',
-          image: 'https://cdn10.bigcommerce.com/s-sr2gbw/products/97/images/10885/Foot-Forms-2020---Crew-Socks--navy__99897.1584467016.1280.1280.jpg?c=2',
-        }
-      ],
-      sizes: [
-        {
-          id: 123,
-          value: 'lg',
-        },
-        {
-          id: 124,
-          value: 'xl',
-        }
-      ],
-    }
+    title: 'Socks',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.',
+    selectedVariant: 0,
+    onSale: true,
+    stockLabel: '',
+    details: [
+      '80 cotton', '20% polyester', 'Gender-neutral'
+    ],
+    variants: [
+      {
+        id: 2234,
+        color: 'is-default',
+        quantity: 5,
+        image: 'https://images.vans.com/is/image/Vans/XRZHTG-HERO?$583x583$',
+        sizes: ['md', 'lg', 'xl'],
+      },
+      {
+        id: 2235,
+        color: 'is-info',
+        quantity: 0,
+        image: 'https://cdn10.bigcommerce.com/s-sr2gbw/products/97/images/10885/Foot-Forms-2020---Crew-Socks--navy__99897.1584467016.1280.1280.jpg?c=2',
+        sizes: ['sm', 'xl'],
+      }
+    ],
+    tabs: [
+      {
+        id: 112,
+        title: 'Details',
+        active: true,
+      }, {
+        id: 113,
+        title: 'Color & Size',
+        active: false,
+      }
+    ],
   },
 
   computed: {
     activeTab() {
-      const activeTab = this.product.tabs.find( tab => !!tab.active);
+      const activeTab = this.tabs.find( tab => !!tab.active);
       return activeTab.title;
     },
 
-    stockLabel() {
-      if (this.product.inventory > 10) {
-        this.product.label = 'In Stock';
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+
+    sizes() {
+      return this.variants[this.selectedVariant].sizes;
+    },
+
+    inventory() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+
+    stockLabelClass() {
+      const inventory = this.variants[this.selectedVariant].quantity;
+
+      if (inventory > 10) {
+        this.stockLabel = 'In Stock';
         return 'is-primary is-light';
-      } else if (this.product.inventory<=10 && this.product.inventory > 0) {
-        this.product.label = 'Almost sold';
+      } else if (inventory<=10 && inventory > 0) {
+        this.stockLabel = 'Almost sold';
         return 'is-warning';
       } else {
-        this.product.label = 'Out of Stock';
+        this.stockLabel = 'Out of Stock';
         return 'is-link';
       }
-    }
+    },
   },
 
   methods: {
     incrementCart() {
-      if (this.cart < this.product.inventory) {
+      if (this.cart < this.inventory) {
         this.cart += 1;
       } else {
-        this.cart = this.product.inventory;
+        this.cart = this.inventory;
       }
     },
 
@@ -86,8 +91,8 @@ new Vue({
       }
     },
 
-    updateProductImage(variantImage) {
-      this.product.assets = variantImage;
-    }
+    updateProduct(index) {
+      this.selectedVariant = index;
+    },
   }
 });
